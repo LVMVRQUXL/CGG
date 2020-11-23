77fc95c2cc92df601c2c1e28aab6d18d5dc899b6
Hervé Huang <herve.huang@yahoo.fr>
2020-11-23 22:25:59 +0100

    feat:[#18]:lipstick: all the rest of the question page

--- app/src/main/java/fr/esgi/rpa/cgg/MainActivity.kt
+++ app/src/main/java/fr/esgi/rpa/cgg/MainActivity.kt
@@ -1,11 +1,22 @@
 package fr.esgi.rpa.cgg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myStartButton: Button? = findViewById(R.id.play_button)
+        myStartButton?.setOnClickListener {
+            val intent = Intent(this,QuestionActivity::class.java)
+            startActivity(intent)
+
+        }
     }
+
+
 }
