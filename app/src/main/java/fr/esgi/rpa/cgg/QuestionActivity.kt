e9eeee18d6ad4ef00af0d546387caeae94c93490
Taiyang999 <56363037+Taiyang999@users.noreply.github.com>
2020-12-29 08:13:02 +0100

    :sparkles: Feature #20
    * add logic for result page
    
    * feat[#20]:construction: 2 adjustments
    
    * feat[#20] construction switch to ResultActivity on button click
    
    Co-authored-by: iReiki <jeanluc.lys@gmail.com>

--- app/src/main/java/fr/esgi/rpa/cgg/QuestionActivity.kt
+++ app/src/main/java/fr/esgi/rpa/cgg/QuestionActivity.kt
@@ -1,7 +1,10 @@
 package fr.esgi.rpa.cgg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.question_page.*
 
 
 class QuestionActivity : AppCompatActivity() {
@@ -9,5 +12,15 @@ class QuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.question_page)
+
+        button1?.setOnClickListener { buttonClicked() }
+        button2?.setOnClickListener { buttonClicked() }
+        button3?.setOnClickListener { buttonClicked() }
+        button4?.setOnClickListener { buttonClicked() }
+    }
+
+    fun buttonClicked() {
+        val intent = Intent(this, ResultActivity::class.java)
+        startActivity(intent)
     }
 }
