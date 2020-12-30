4a036e80b84b14239acfc19533b609e0ac7765af
Loïc Lamarque <loiclamarque777@gmail.com>
2020-12-30 10:17:30 +0100

    refactor: [#31] :fire: Remove useless import in color's activity

--- app/src/main/java/fr/esgi/rpa/cgg/color/ColorsActivity.kt
+++ app/src/main/java/fr/esgi/rpa/cgg/color/ColorsActivity.kt
@@ -13,7 +13,6 @@ import kotlinx.android.synthetic.main.activity_colors.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.util.*
 
 class ColorsActivity : AppCompatActivity() {
     private var colors: MutableList<Color> = mutableListOf()
