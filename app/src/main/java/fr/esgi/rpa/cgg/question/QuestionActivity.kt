670c23f3e9f2f4977954b9ff3041aceda42c8dac
Loïc Lamarque <loiclamarque777@gmail.com>
2021-01-02 21:56:50 +0100

    refactor: [#36] :recycle: Refactor code after rebasing

--- app/src/main/java/fr/esgi/rpa/cgg/question/QuestionActivity.kt
+++ app/src/main/java/fr/esgi/rpa/cgg/question/QuestionActivity.kt
@@ -1,77 +1,69 @@
 package fr.esgi.rpa.cgg.question
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import fr.esgi.rpa.cgg.R
 import fr.esgi.rpa.cgg.result.ResultActivity
 import kotlinx.android.synthetic.main.activity_question.*
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
-import kotlinx.android.synthetic.main.question_page.*
-
 
 class QuestionActivity : AppCompatActivity() {
-    private var round_number = 1
-    private var difficulty = 5
-    var score = 0
+    private val suggestionButtons: MutableList<Button?> = mutableListOf()
+    private val roundsNumber: Int = 5
+    private var currentRound: Int = 1
+    private var score = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
 
-        round?.text = difficulty.toString()
-        currentRound?.text = " $round_number"
+        this.suggestionButtons.add(button1)
+        this.suggestionButtons.add(button2)
+        this.suggestionButtons.add(button3)
+        this.suggestionButtons.add(button4)
 
-        val buttons = arrayOf(button1, button2, button3, button4)
-        next_result?.setOnClickListener { nextClicked(next_result, currentRound, buttons) }
-        button1?.setOnClickListener { buttonClicked(button1, buttons) }
-        button2?.setOnClickListener { buttonClicked(button2, buttons) }
-        button3?.setOnClickListener { buttonClicked(button3, buttons) }
-        button4?.setOnClickListener { buttonClicked(button4, buttons) }
+        rounds_number?.text = this.roundsNumber.toString()
+        current_round?.text = this.currentRound.toString()
+
+        next?.setOnClickListener { this.nextClicked() }
+        for (suggestion in this.suggestionButtons)
+            suggestion?.setOnClickListener { buttonClicked ->
+                this.buttonClicked(buttonClicked as Button)
+            }
     }
 
-    private fun buttonClicked() {
-        val intent = Intent(this, ResultActivity::class.java)
-        startActivity(intent)
-        fun buttonClicked(v: Button, buttons: Array<Button>) {
-            if (R.id.button1 == v.id) { // Right Answer
-                v.setTextColor(Color.GREEN)
-                score += 1
-                Log.d(QuestionActivity::class.simpleName, "RIGHT  $round_number")
-            } else { // Wrong Answer
-                Log.d(QuestionActivity::class.simpleName, "WRONG  $round_number")
-                v.setTextColor(Color.RED)
-                for (item in buttons) {
-                    if (item.id == R.id.button1) {
-                        item.setTextColor(Color.GREEN)
-                    }
-                }
-            }
-            for (item in buttons) {
-                item.isEnabled = false
-                item.isClickable = false
-            }
+    private fun buttonClicked(button: Button) {
+        if (button1?.id == button.id) {
+            button.setTextColor(Color.GREEN)
+            this.score++
+        } else {
+            button.setTextColor(Color.RED)
+            for (suggestion in this.suggestionButtons)
+                if (button1?.id == suggestion?.id) suggestion?.setTextColor(Color.GREEN)
         }
-
-        fun nextClicked(v: Button, round: TextView, buttons: Array<Button>) {
-            round_number += 1
-
-            if (round_number > difficulty) {
-                val intent = Intent(this, ResultActivity::class.java)
-                startActivity(intent)
-            } else {
-                round.text = " " + round_number.toString()
-                for (item in buttons) {
-                    item.isEnabled = true
-                    item.isClickable = true
-                    item.setTextColor(R.color.colorPrimaryDark)
-                }
-            }
-            if (round_number == difficulty) v.text = "Result"
+        for (suggestion in this.suggestionButtons) {
+            suggestion?.isEnabled = false
+            suggestion?.isClickable = false
         }
     }
+
+    private fun nextClicked() {
+        this.currentRound++
+        if (this.roundsNumber < this.currentRound) {
+            val intent = Intent(this, ResultActivity::class.java)
+            startActivity(intent)
+        } else {
+            current_round?.text = this.currentRound.toString()
+            for (suggestion in this.suggestionButtons) {
+                suggestion?.isEnabled = true
+                suggestion?.isClickable = true
+                val color = ContextCompat.getColor(this, R.color.colorPrimaryDark)
+                suggestion?.setTextColor(color)
+            }
+            if (this.roundsNumber == this.currentRound) next?.text = getString(R.string.result)
+        }
+    }
+}
